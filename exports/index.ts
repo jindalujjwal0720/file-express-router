b8e7158045177b1f3b5b7d7eef42126710bd9fb2
jindalujjwal0720 <jindalujjwal0720@gmail.com>
2025-01-31 01:42:50 +0530

    Deploy website - based on a4034b54f8e4318f6a5e5d8bfd93c6af7d00bdf2

--- exports/index.ts
+++
@@ -1,3 +0,0 @@
-import { router } from '../src/utils/router';
-
-export default router;
--- src/dev-routes/[path]/home.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-const handleGetHome = async (req: Request, res: Response) => {
-  const { path } = req.params;
-  console.log('path', path);
-  res.json({ path, message: 'Hello World!' });
-};
-
-export const get = handleGetHome;
